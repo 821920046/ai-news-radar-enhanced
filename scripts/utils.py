ac232e9f98e32c0ddc0391d4a8c0b524786175e4
Windwalker <38275925+821920046@users.noreply.github.com>
2026-08-10 13:48:19 +0800

    Delete scripts/utils.py

--- scripts/utils.py
+++
@@ -1,3 +0,0 @@
-"""Shim: re-exports from core.utils for backward compatibility."""
-from core.utils import *  # noqa: F401, F403
-from core.utils import _env_int  # noqa: F401 (private but used by ai_processor)
